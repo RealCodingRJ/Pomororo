375df5b983911ffde803985387d5e072f4fa75eb
Ryan James <realcoder2023@gmail.com>
2025-05-29 13:31:38 -0400

    added a shape CIRCLE To Main Page?

--- app/src/main/java/com/example/pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -4,23 +4,25 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -44,6 +46,18 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
+    @Composable
+    fun DrawCircle() {
+        Column(
+            modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center)
+                .padding(top = 200.dp)
+        ) {
+            Box(modifier = Modifier.size(150.dp)
+                .clip(CircleShape).background(Color.Black))
+        }
+
+    }
     @Composable
     fun Footer() {
         Row(
@@ -97,6 +111,7 @@ class MainActivity : ComponentActivity() {
 
             TITLE()
             AlignTextSpaced()
+            DrawCircle()
             CreateButton()
             Footer()
 
@@ -107,10 +122,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun ThreadsSleep() {
 
-
         Thread.sleep(1000);
-
-
     }
 
     @Composable
